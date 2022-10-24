a2b9dddb7652fc88d27427f1e0c10e8dc7a6b7b3
aprimakov <primakov.aleksandr@otr.ru>
2022-10-24 19:07:08 +0400

    absolutelu wrong commit

--- src/main/java/com/dailycodebuffer/springbootfileupload/model/Model.java
+++ src/main/java/com/dailycodebuffer/springbootfileupload/model/Model.java
@@ -3,13 +3,5 @@ package com.dailycodebuffer.springbootfileupload.model;
 public class Model {
     private String stringForCherryPickInFirstBranch;
     private int x =4;
-    private String str;
-    private String myOwn;
-    private String forStash;
-    private int d;
-    private int dsdf;
-    private int dsdfsdv;
-    private int vsdvdsdsdfsdv;
-    private String patch;
-    private int hjbdsdfsdv;
+    private String s="Это должно уйти при реверте! ";
 }
